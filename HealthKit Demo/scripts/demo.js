063ec338f19cff967e1c31d9e77dc338bc27d04a
Eddy Verbruggen <eddyverbruggen@gmail.com>
2015-01-17 15:37:59 +0000

    demo++

--- HealthKit Demo/scripts/demo.js
+++ HealthKit Demo/scripts/demo.js
@@ -4,10 +4,153 @@
 
     DemoViewModel = kendo.data.ObservableObject.extend({
 
-        todoFunctionname: function () {
+        isAvailable: function () {
             if (!this.checkSimulator()) {
-              // todo
+              window.plugins.healthkit.available(
+                function(isAvailable) { alert(isAvailable ? "Yes :)" : "Nope :(") }
+              );
             }
+        },
+
+        getBirthdate: function () {
+            if (!this.checkSimulator()) {
+              window.plugins.healthkit.readDateOfBirth(
+                this.onSuccess,
+                this.onError
+              );
+            }
+        },
+
+        getGender: function () {
+            if (!this.checkSimulator()) {
+              window.plugins.healthkit.readGender(
+                this.onSuccess,
+                this.onError
+              );
+            }
+        },
+
+        getHeight: function () {
+            if (!this.checkSimulator()) {
+              window.plugins.healthkit.readHeight(
+                {
+                  'unit': 'cm' // mm|cm|m|in|ft
+                },
+                this.onSuccess,
+                this.onError
+              );
+            }
+        },
+
+        getWeight: function () {
+            if (!this.checkSimulator()) {
+              window.plugins.healthkit.readWeight(
+                {
+                  'unit': 'kg' // g|kg|oz|lb|st
+                },
+                this.onSuccess,
+                this.onError
+              );
+            }
+        },
+
+        setWeight80kg: function () {
+            if (!this.checkSimulator()) {
+              window.plugins.healthkit.saveWeight(
+                {
+                  'unit'  : 'kg', // g|kg|oz|lb|st
+                  'amount': 80,
+                  'date'  : new Date() // is 'now', which is the default as well
+                },
+                this.onSuccess,
+                this.onError
+              );
+            }
+        },
+
+        setWeight80kgToday: function () {
+            if (!this.checkSimulator()) {
+              window.plugins.healthkit.saveWeight(
+                {
+                  'unit'  : 'kg', // g|kg|oz|lb|st
+                  'amount': 80,
+                  'date'  : new Date() // is 'now', which is the default as well
+                },
+                this.onSuccess,
+                this.onError
+              );
+            }
+        },
+
+        setWeight170poundYesterday: function () {
+            if (!this.checkSimulator()) {
+              window.plugins.healthkit.saveWeight(
+                {
+                  'unit'  : 'lb', // g|kg|oz|lb|st
+                  'amount': 170,
+                  'date'  : new Date(new Date().getTime() - 24*60*60*1000) // yesterday
+                },
+                this.onSuccess,
+                this.onError
+              );
+            }
+        },
+
+        setHeight180cmToday: function () {
+            if (!this.checkSimulator()) {
+              window.plugins.healthkit.saveHeight(
+                {
+                  'unit': 'cm', // m|cm|mm|in|ft
+                  'amount': 180
+                  // leaving out the optinal 'date' param, so it defaults to 'now'
+                },
+                this.onSuccess,
+                this.onError
+              );
+            }
+        },
+
+        querySampleType: function () {
+          if (!this.checkSimulator()) {
+            window.plugins.healthkit.querySampleType(
+              {
+                'startDate': new Date(new Date().getTime() - 2*24*60*60*1000), // two days ago
+                'endDate': new Date(), // now
+                'sampleType': 'HKQuantityTypeIdentifierStepCount', // anything in HealthKit/HKTypeIdentifiers.h
+                'unit' : 'count' // make sure this is compatible with the sampleType
+              },
+              this.onSuccess,
+              this.onError
+            );
+          }
+        },
+
+        saveWorkout: function () {
+          if (!this.checkSimulator()) {
+            window.plugins.healthkit.saveWorkout(
+              {
+                'activityType': 'HKWorkoutActivityTypeCycling', // HKWorkoutActivityType constant (https://developer.apple.com/library/ios/documentation/HealthKit/Reference/HKWorkout_Class/#//apple_ref/c/tdef/HKWorkoutActivityType)
+                'quantityType': 'HKQuantityTypeIdentifierDistanceCycling',
+                'startDate': new Date(), // mandatory
+                'endDate': null,         // optional, use either this or duration
+                'duration': 60 * 60,     // in seconds, optional, use either this or endDate
+                'energy': 800,
+                'energyUnit': 'kcal',    // J|cal|kcal
+                'distance': 25,          // optional
+                'distanceUnit': 'km'
+              },
+              this.onSuccess,
+              this.onError
+            )
+          }
+        },
+
+        onSuccess: function (result) {
+          alert(result == null ? "OK" : JSON.stringify(result));
+        },
+
+        onError: function (result) {
+          alert(JSON.stringify(result));
         },
 
         checkSimulator: function() {
